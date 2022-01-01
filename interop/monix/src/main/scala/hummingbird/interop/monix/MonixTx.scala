fe54504f97442ac1ea0f0efac70756e3f22f7431
busti <oss@busti.cool>
2022-01-01 06:21:38 +0100

    partially implement missing methods

--- interop/monix/src/main/scala/hummingbird/interop/monix/MonixTx.scala
+++ interop/monix/src/main/scala/hummingbird/interop/monix/MonixTx.scala
@@ -1,8 +1,12 @@
 package hummingbird.interop.monix
 
+import cats.effect.Effect
 import hummingbird._
 import monix.eval._
 import monix.reactive._
+
+import scala.concurrent.Future
+import scala.concurrent.duration.FiniteDuration
 
 class MonixTx[+A](val stream: Observable[A]) extends Tx[A] {
   type F[+T] = Task[T]
@@ -24,6 +28,24 @@ class MonixTx[+A](val stream: Observable[A]) extends Tx[A] {
 
   def withLatestMap[B, C](other: MonixTx[B])(fn: (A, B) => C): MonixTx[C] =
     new MonixTx(stream.withLatestFrom(other.stream)(fn))
+
+  def scan[B](z: B)(op: (B, A) => B): MonixTx[B] = new MonixTx(stream.scan(z)(op))
+
+  def scan0[B](z: B)(op: (B, A) => B): MonixTx[B] = new MonixTx(stream.scan0(z)(op))
+
+  def debounce(d: FiniteDuration): MonixTx[A] = new MonixTx(stream.debounce(d))
+
+  def debounceMillis(millis: Long): MonixTx[A] = new MonixTx(stream.debounce(FiniteDuration.apply(millis, "millis")))
+
+  def async: MonixTx[A] = new MonixTx(stream.asyncBoundary(OverflowStrategy.Unbounded))
+
+  def delay(duration: FiniteDuration): MonixTx[A] = new MonixTx(stream.delayExecution(duration))
+
+  def delayMillis(millis: Long): MonixTx[A] = new MonixTx(stream.delayExecution(FiniteDuration.apply(millis, "millis")))
+
+  def concatMapFuture[B](fn: A => Future[B]): MonixTx[B] = ??? // new MonixTx(stream.concatMap(Observable.fromFuture(() => fn())))
+
+  def concatMapAsync[FF[_] : Effect, B](fn: A => FF[B]): MonixTx[B] = ??? // new MonixTx(stream.concatMap(Observable.fromFuture(() => fn())))
 }
 
 class MonixTxBuilder extends TxBuilder {
